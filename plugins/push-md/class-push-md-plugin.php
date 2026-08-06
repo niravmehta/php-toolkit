874a6f1e42ea801b32c233c3fa19af0136a2d066
Nirav Mehta <nirav@mehtanirav.com>
2026-08-07 02:04:53 +0530

    fix(push-md): fix parameter signature of validate_post_frontmatter_references to pass options

--- plugins/push-md/class-push-md-plugin.php
+++ plugins/push-md/class-push-md-plugin.php
@@ -5786,7 +5786,7 @@ class Push_MD_Plugin {
 		return $val;
 	}
 
-	private static function validate_post_frontmatter_references( $metadata, $post_type ) {
+	private static function validate_post_frontmatter_references( $metadata, $post_type, $options = array() ) {
 		unset( $post_type );
 
 		if ( isset( $metadata['author'] ) && '' !== trim( (string) $metadata['author'] ) ) {
